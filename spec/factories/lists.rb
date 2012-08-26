f4bb22e471339a8d4c73d51da022529ad85a3b06
Rystraum Gamonez <rystraum@gmail.com>
2012-08-26 23:29:28 +0800

    List model

---
+++ spec/factories/lists.rb
@@ -0,0 +1,9 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :list do
+    name "MyString"
+    description "MyText"
+    user nil
+  end
+end
