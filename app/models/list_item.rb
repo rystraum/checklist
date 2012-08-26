23c6fed6ef26faa690a4c2ec12726eb7dae50180
Rystraum Gamonez <rystraum@gmail.com>
2012-08-27 00:05:29 +0800

    answer kind enum

--- app/models/list_item.rb
+++ app/models/list_item.rb
@@ -1,5 +1,9 @@
 class ListItem < ActiveRecord::Base
   belongs_to :list
   attr_accessible :description, :title, :answer_kind
+
+  def answer_kind_enum
+    ['yes/no','longtext','selection']
+  end
 end
 
