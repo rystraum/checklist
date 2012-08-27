8d1aa9b6081497953d96d035b64f3fa2067abe60
Rystraum Gamonez <rystraum@gmail.com>
2012-08-27 10:03:39 +0800

    Converted lists controller to regular controller instead of inherited resources

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,42 @@
-class ListsController < InheritedResources::Base
+class ListsController < ApplicationController
   before_filter :authenticate_inspector!
+  before_filter :get_lists, only: [:index]
+  before_filter :get_list, only: [:show, :edit, :update, :destroy]
+  respond_to :html, :json
+
+  def index
+    respond_with @lists
+  end
+
+  def new
+    @list = List.new
+    respond_with @list
+  end
+
+  def create
+    @list = List.new params[:list]
+    redirect_to lists_path if @list.save
+  end
+
+  def edit
+    respond_with @list
+  end
+
+  def update
+    redirect_to @list, notice: "List updated" if @list.save
+  end
+
+  def destroy
+    redirect_to lists_path if @list.destroy
+  end
+
+  protected
+    def get_list
+      @list = List.find params[:id]
+    end
+
+    def get_lists
+      @lists = List.all
+    end
 end
 
